8aaa2dfd6c8554582e583b8bb48f5bace4a1624d
David S <dave.techdev@gmail.com>
2021-06-30 23:22:29 +0700

    Add basic dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:todolist_app/routes.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(TodoListApp());
@@ -8,12 +12,25 @@ class TodoListApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TodoListApp',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ScreenUtilInit(
+      designSize: ScreenUtil.defaultSize,
+      builder: () => MaterialApp(
+        builder: (context, widget) {
+          return MediaQuery(
+            //Setting font does not change with system font size
+            data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+            child: widget ?? Container(),
+          );
+        },
+        debugShowCheckedModeBanner: false,
+        title: 'ToDoList App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          //textTheme: TextTheme(button: TextStyle(fontSize: 45.sp)),
+        ),
+        initialRoute: '/',
+        routes: routes,
       ),
-      home: Scaffold(),
     );
   }
 }
---
+++ lib/routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+//import 'package:todolist_app/screens/screen1.dart'; //todo
+
+final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+  // '/': (BuildContext context) => Screen1(),
+  // '/registration': (BuildContext context) => RegistrationScreen(),
+  // '/login': (BuildContext context) => LoginScreen(),
+  // '/chat': (BuildContext context) => ChatScreen()
+};
